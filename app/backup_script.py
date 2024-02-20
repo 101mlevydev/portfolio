afdf0c5f7bbb8d3e706c5cb8b46931bc9b4b44d1
Gogs <gogs@fake.local>
2024-02-20 06:33:27 -0500

    script works properly

--- app/backup_script.py
+++ app/backup_script.py
@@ -14,21 +14,22 @@ def read_conf_file():
     files_to_backup = ["/etc/hosts", "/etc/resolv.conf"]
     with open(CONFIG_FILE, "r") as config_file:
         for line in config_file:
-            file_path = line.strip()  # Remove newline characters
+            file_path = line.strip()  
             if os.path.exists(file_path):
                 files_to_backup.append(file_path)
             else:
                 print(f"File '{file_path}' does not exist. Moving to the next file.")
-    return files_to_backup  # Return the updated list of files to backup
+    return files_to_backup  
 
 def backup():
     timestamp = time.strftime("%Y|%m|%d|%H|%M|%S")
-    files_to_backup = read_conf_file()  # Call read_conf_file inside backup() to get updated file list
-    for source_file in files_to_backup:  # Iterate over files_to_backup list
+    files_to_backup = read_conf_file()  
+    for source_file in files_to_backup:  
         try:
             filename = os.path.basename(source_file)
-            destination = os.path.join(BACKUP_DIR, f"{filename}_{timestamp}")  # Corrected the string formatting
+            destination = os.path.join(BACKUP_DIR, f"{filename}_{timestamp}")
             shutil.copy(source_file, destination)
+            print(f"{filename} successfuly backked up")
         except Exception as e:
             print(f"Warning: Backup of {source_file} failed. {str(e)}")
             with open(os.path.join(BACKUP_DIR, "failure.log"), "a") as log_file:
